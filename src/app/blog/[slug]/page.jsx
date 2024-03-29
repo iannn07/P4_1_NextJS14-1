18d76178eef8afd953fcbf40454a134c145796bb
Iannn <pristianb007@gmail.com>
2024-03-29 21:28:27 +0700

    Done Design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,10 +1,52 @@
+import styles from './post.module.css';
+import Image from 'next/image';
+
 const SinglePostPage = () => {
-    return (
-      <div>
-        Single Post Page
+  return (
+    <div className={styles.container}>
+      <div className={styles['img-container']}>
+        <Image
+          src={
+            'https://images.pexels.com/photos/3259624/pexels-photo-3259624.jpeg?auto=compress&cs=tinysrgb&w=600'
+          }
+          alt=''
+          fill
+          className={styles.img}
+        />
       </div>
-    )
-  }
-  
-  export default SinglePostPage
-  
+      <div className={styles['text-container']}>
+        <h1 className={styles.title}>Gausspy</h1>
+        <div className={styles.detail}>
+          <Image
+            src={
+              'https://images.pexels.com/photos/3259624/pexels-photo-3259624.jpeg?auto=compress&cs=tinysrgb&w=600'
+            }
+            alt=''
+            width={50}
+            height={50}
+            className={styles.avatar}
+          />
+          <div className={styles['detail-text']}>
+            <span className={styles['detail-title']}>Author</span>
+            <span className={styles['detail-value']}>Gausspy</span>
+          </div>
+          <div className={styles['detail-text']}>
+            <span className={styles['detail-title']}>Published</span>
+            <span className={styles['detail-value']}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            condimentum est non lectus dictum rhoncus. Pellentesque habitant
+            morbi tristique senectus et netus et malesuada fames ac turpis
+            egestas. Etiam convallis erat sed varius sodales. Nunc libero nisi,
+            tempus sit amet enim sed, elementum viverra mauris.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,23 @@
+import PostCard from '@/components/post-card/post-card';
+import styles from './blog.module.css';
+
 const BlogPage = () => {
   return (
-    <div>
-      Blog Page
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
