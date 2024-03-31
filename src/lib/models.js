01c4ea37318ea49fe74ba9fb2ec854ab2ed0922e
Iannn <pristianb007@gmail.com>
2024-03-31 20:15:20 +0700

    GitHub OAuth & Register it to MongoDB

--- src/lib/models.js
+++ src/lib/models.js
@@ -2,6 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
     username: {
       type: String,
       required: true,
